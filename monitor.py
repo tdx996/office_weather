aea82a4bfeb41bba02fb1c648aff2688729bf65c
Tim Lossen <tim@lossen.de>
2015-07-06 10:13:22 +0200

    give credit to original author

--- monitor.py
+++ monitor.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 
+# based on code by henryk ploetz
+# https://hackaday.io/project/5301-reverse-engineering-a-low-cost-usb-co-monitor/log/17909-all-your-base-are-belong-to-us
+
 import sys, fcntl, time, librato, yaml
 
 def decrypt(key,  data):
