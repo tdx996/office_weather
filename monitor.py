cdca183d60b58dd1c61153e3f5b7f5d9343265fc
Robert Habermann <mail@rhab.de>
2015-10-29 21:40:48 +0100

    add cmd line arg for config file (with defaulting to ./config.yaml)
    Add a command line argument for (full) path to the config.yaml file.
    Before this the config.yaml was hardcoded to "config.yaml".
    Now a custom config file can be used by specifying it as a second
    commandline argument.  Will default to "script_base_dir/config.yaml".

--- monitor.py
+++ monitor.py
@@ -8,27 +8,27 @@ import sys, fcntl, time, librato, yaml
 def decrypt(key,  data):
     cstate = [0x48,  0x74,  0x65,  0x6D,  0x70,  0x39,  0x39,  0x65]
     shuffle = [2, 4, 0, 7, 1, 6, 5, 3]
-    
+
     phase1 = [0] * 8
     for i, o in enumerate(shuffle):
         phase1[o] = data[i]
-    
+
     phase2 = [0] * 8
     for i in range(8):
         phase2[i] = phase1[i] ^ key[i]
-    
+
     phase3 = [0] * 8
     for i in range(8):
         phase3[i] = ( (phase2[i] >> 3) | (phase2[ (i-1+8)%8 ] << 5) ) & 0xff
-    
+
     ctmp = [0] * 8
     for i in range(8):
         ctmp[i] = ( (cstate[i] >> 4) | (cstate[i]<<4) ) & 0xff
-    
+
     out = [0] * 8
     for i in range(8):
         out[i] = (0x100 + phase3[i] - ctmp[i]) & 0xff
-    
+
     return out
 
 def hd(d):
@@ -44,8 +44,15 @@ def publish(client, prefix, co2, tmp):
     except:
         print "Unexpected error:", sys.exc_info()[0]
 
-def config():
-    with open("config.yaml", 'r') as stream:
+def config(config_file=None):
+    """Get config from file; if no config_file is passed in as argument
+        default to "config.yaml" in script dir"""
+
+    if config_file is None:
+        script_base_dir = os.path.dirname(os.path.realpath(sys.argv[0])) + "/"
+        config_file = script_base_dir + "config.yaml"
+
+    with open(config_file, 'r') as stream:
         return yaml.load(stream)
 
 def client(config):
@@ -53,15 +60,20 @@ def client(config):
 
 
 if __name__ == "__main__":
-    key = [0xc4, 0xc6, 0xc0, 0x92, 0x40, 0x23, 0xdc, 0x96]    
+    key = [0xc4, 0xc6, 0xc0, 0x92, 0x40, 0x23, 0xdc, 0x96]
     fp = open(sys.argv[1], "a+b",  0)
     HIDIOCSFEATURE_9 = 0xC0094806
     set_report = "\x00" + "".join(chr(e) for e in key)
     fcntl.ioctl(fp, HIDIOCSFEATURE_9, set_report)
-    
+
     values = {}
     stamp = now()
-    config = config()
+
+    try:
+        config = config(config_file=sys.argv[2])
+    except IndexError:
+        config = config()
+
     client = client(config)
 
     while True:
@@ -73,12 +85,12 @@ if __name__ == "__main__":
             op = decrypted[0]
             val = decrypted[1] << 8 | decrypted[2]
             values[op] = val
-            
+
             if (0x50 in values) and (0x42 in values):
                 co2 = values[0x50]
                 tmp = (values[0x42]/16.0-273.15)
                 print "CO2: %4i TMP: %3.1f" % (co2, tmp)
                 if now() - stamp > 5:
                     print ">>>"
-                    publish(client, config["prefix"], co2, tmp) 
+                    publish(client, config["prefix"], co2, tmp)
                     stamp = now()
